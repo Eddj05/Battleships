5fa7d418cf7e73c4f9ea9824a4ef3509194af759
KennieSlagter <k.m.slagter@student.rug.nl>
2024-05-28 12:08:00 +0200

    adding necessary files and templates to the repository

---
+++ tpl/head.php
@@ -0,0 +1,46 @@
+<?php
+// P_Print function
+function p_print($array){
+    echo "<pre>";
+    print_r($array);
+    echo "</pre>";
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Meta Data -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title><?= $page_title ?></title>
+
+    <!-- Styles -->
+    <link rel="preconnect" href="https://cdnjs.cloudflare.com">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.1/css/bootstrap.min.css" integrity="sha512-T584yQ/tdRR5QwOpfvDfVQUidzfgc2339Lc8uBDtcp/wYu80d7jwBgAxbyMh0a9YM9F8N3tdErpFI8iaGx6x5g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- Scripts -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+</head>
+<body>
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">WP21 Assignment 3</a>
+        <ul class="navbar-nav mr-auto">
+            <?php $active = $navigation['active']; ?>
+            <?php foreach($navigation['items'] as $title => $url){
+                if ($title == $active){ ?>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="<?= $url ?>"><?= $title ?></a>
+                    </li>
+                <?php } else {?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?= $url ?>"><?= $title ?></a>
+                    </li>
+                <?php } ?>
+            <?php } ?>
+        </ul>
+    </nav>
+</header>
