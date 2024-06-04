f7dacf1b0696b024fd2033cc1137fb5a4c18dd00
KennieSlagter <k.m.slagter@student.rug.nl>
2024-06-04 12:53:11 +0200

    adding flip function to app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+const optionContainer = document.querySelector(`.option-container`)
+const flipButton = document.querySelector('#flip-button')
+
+function flip() {
+    const optionShips = Array.from(optionContainer.children)
+    optionShips.forEach(optionShip => optionShip.style.transform = 'rotate(90deg)')
+}
+
+flipButton.addEventListener('click', flip)
