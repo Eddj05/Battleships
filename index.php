de39a83c79269427689d595ce0a92443ab6cd639
Carlijnvrieling <154004492+Carlijnvrieling@users.noreply.github.com>
2024-06-06 14:34:28 +0200

    Changed the title of the rules page

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <title>Start Battleships</title>
+        <title>Rules Battleships</title>
         <link rel="preconnect" href="https://cdnjs.cloudflare.com">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.1/css/bootstrap.min.css" integrity="sha512-T584yQ/tdRR5QwOpfvDfVQUidzfgc2339Lc8uBDtcp/wYu80d7jwBgAxbyMh0a9YM9F8N3tdErpFI8iaGx6x5g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <link rel="stylesheet" href="css/style.css">
