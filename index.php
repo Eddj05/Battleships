b63535f5e19a9295c44eb9b719f0fcd4fde5fdc0
Carlijnvrieling <154004492+Carlijnvrieling@users.noreply.github.com>
2024-06-14 11:27:32 +0200

    Added the battleships header to body_start.php

--- index.php
+++ index.php
@@ -7,14 +7,7 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.1/css/bootstrap.min.css" integrity="sha512-T584yQ/tdRR5QwOpfvDfVQUidzfgc2339Lc8uBDtcp/wYu80d7jwBgAxbyMh0a9YM9F8N3tdErpFI8iaGx6x5g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <link rel="stylesheet" href="css/style.css">
     </head>
-    <body>
-    <div id="header">
-        <div class="header-content">
-            <img src="Images/battleship2.png" alt="Battleship">
-            <a href="index.php" class="homepage"> <h1>Battleships</h1> </a>
-            <img src="Images/battleship2_reverse2.png" alt="Battleship">
-        </div>
-    </div>
+    <?php include "tpl/body_start.php" ;?>
         <h3> Welcome to Battleships! </h3>
         <h4> Press the button to start the game or view the rules!</h4>
         <div id="startup">
--- tpl/body_start.php
+++ tpl/body_start.php
@@ -1,1 +1,8 @@
-<div class="container">
+<body>
+    <div id="header">
+        <div class="header-content">
+            <img src="Images/battleship2.png" alt="Battleship">
+            <a href="index.php" class="homepage"> <h1>Battleships</h1> </a>
+            <img src="Images/battleship2_reverse2.png" alt="Battleship">
+        </div>
+    </div>
