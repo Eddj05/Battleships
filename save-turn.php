7ba83136cd4a842751ed6f7957cdeecefbbf98ab
KennieSlagter <k.m.slagter@student.rug.nl>
2024-06-20 20:06:07 +0200

    adding save-turn.php

---
+++ save-turn.php
@@ -0,0 +1,12 @@
+<?php
+header('Content-Type: application/json');
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $jsonFile = 'turn.json';
+
+    $data = json_decode(file_get_contents("php://input"), true);
+    $jsonData = file_get_contents($jsonFile);
+    file_put_contents($jsonFile, json_encode($data, JSON_PRETTY_PRINT));
+
+
+}
